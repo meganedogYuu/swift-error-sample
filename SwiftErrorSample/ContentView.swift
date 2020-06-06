6510cc8ebdcbe2aa2ac84af5de8d26cdb9f0b6a9
meganedogYuu <tkra.yuuki@gmail.com>
2020-06-06 11:43:52 +0900

    Initial Commit

---
+++ SwiftErrorSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftErrorSample
+//
+//  Created by yuu on 2020/06/06.
+//  Copyright © 2020 yuu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
